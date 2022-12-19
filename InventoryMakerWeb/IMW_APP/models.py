09d4b0bcb15900ee1a5a8fe937211efba77f3ca1
Valentino Amato <valentinoamatoroberts@gmail.com>
2022-12-19 10:32:22 -0300

    Added column to inventory table.
    An inventory can now have an optional description.

--- InventoryMakerWeb/IMW_APP/models.py
+++ InventoryMakerWeb/IMW_APP/models.py
@@ -3,22 +3,31 @@ from django.conf import settings
 
 # Create your models here.
 class inventory(models.Model):
-    name = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    description = models.CharField(max_length=1000,null=True)
 
     def __str__(self):
         return f"{self.name}-{self.user}"
         
-    def GetNames(ivt):
+    def GetName(ivt):
         return ivt.name
     
-    def GetIds(ivt):
+    def GetId(ivt):
         return ivt.id
 
 class items(models.Model):
     ivt = models.ForeignKey(inventory, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000,null=True)
+    quantity = models.IntegerField(null=True)
+    unity = models.CharField(max_length=50,null=True)
+    
+    def GetName(itm):
+        return itm.name
+    
+    def GetId(itm):
+        return itm.id
 
 
 
